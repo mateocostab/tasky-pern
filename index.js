123bfe72da3e54d6dbf11006003070fbe7884ed3
Mateo <mateocosta@Mateo.localdomain>
2021-11-06 16:40:33 -0500

    ready to deploy

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const cors = require("cors");
 const pool = require("./db");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-
-//process.env.PORT
+require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
 if (process.env.NODE_ENV === "production") {
   //server static content
   //npm run build
